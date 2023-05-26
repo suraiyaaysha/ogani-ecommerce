b3e313502ef7987b4a6d736b35476589e04d699b
Suraiya Aysha <suraiyaaysha.cse@gmail.com>
2023-05-26 10:26:13 +0600

    feat: Contact - Factory, Seeder done

--- database/migrations/2023_05_24_142425_create_contacts_table.php
+++ database/migrations/2023_05_24_142425_create_contacts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->text('message');
-            $table->foreignId('user_id')->constrained();
+            // $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
