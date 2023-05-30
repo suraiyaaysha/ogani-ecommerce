c4ebbc5e140696c95b52e6dd3ff5f8d4567f916a
Suraiya Aysha <suraiyaaysha.cse@gmail.com>
2023-05-30 18:08:49 +0600

    chor: admin profile photo add into admin navbar and sidebar

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -68,7 +68,8 @@
             <li class="nav-item dropdown">
                 <a class="nav-link" id="profileDropdown" href="#" data-bs-toggle="dropdown">
                     <div class="navbar-profile">
-                        <img class="img-xs rounded-circle" src="assets/images/faces/face15.jpg" alt="">
+                        {{-- <img class="img-xs rounded-circle" src="assets/images/faces/face15.jpg" alt=""> --}}
+                        <img class="img-xs rounded-circle" src="{{ asset(url(auth()->user()->profile_photo)) }}" alt="">
                         <p class="mb-0 d-none d-sm-block navbar-profile-name">{{ Auth::user()->first_name }}</p>
                         <i class="mdi mdi-menu-down d-none d-sm-block"></i>
                     </div>
