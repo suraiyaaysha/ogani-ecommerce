aa07e2ca8018752550266b2e6e20f4a1bd03198c
Suraiya Aysha <suraiyaaysha.cse@gmail.com>
2023-06-04 03:16:38 +0600

    refactor: removed unnecessary code from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 // For adding globally header category variable, So that I don't have to call the variable again & again.
 use App\Models\ProductCategory;
-use App\Models\Product;
+// use App\Models\Product;
 use App\Models\Blog;
 use App\Models\BlogCategory;
 use App\Models\Tag;
@@ -41,30 +41,6 @@ class AppServiceProvider extends ServiceProvider
             $view->with('categories', ProductCategory::all());
         });
 
-        // Share categories variable with index view
-        View::composer('frontend.index', function ($view) {
-            $view->with('categories', ProductCategory::all());
-
-            // Share categoriesHasFeaturedProducts variable with index view
-            $view->with('categoriesHasFeaturedProducts',ProductCategory::whereHas('products', function ($query) {
-                $query->where('is_featured', true);
-                })->get());
-
-            $view->with('featuredProducts', Product::where('is_featured', true)->get());
-            $view->with('latestProducts', Product::get());
-            $view->with('reviewedProducts', Product::has('reviews')->get());
-        });
-
-        // share allBlog variable with frontend.blog view
-        View::composer('frontend.blog.index', function ($view) {
-            // $view->with('allBlog', Blog::all());
-            $view->with('latestBlog', Blog::latest()->take(3)->get());
-            $view->with('blogs', Blog::latest()
-                    ->paginate(6));
-
-            $view->with('blogCategories', BlogCategory::all());
-        });
-
         // });
         View::composer('frontend.partials.blog-aside', function ($view) {
             $view->with('blogCategories', BlogCategory::all());
@@ -73,7 +49,6 @@ class AppServiceProvider extends ServiceProvider
         });
 
         // Add pagination
-        // Paginator::defaultView('vendor.pagination.bootstrap-4');
         Paginator::useBootstrap();
 
     }
