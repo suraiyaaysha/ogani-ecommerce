40b6ef6c672173cc65809f22824482584c79d414
Suraiya Aysha <suraiyaaysha.cse@gmail.com>
2023-06-05 12:57:29 +0600

    chor: remove unwanted code from shop index page

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -54,6 +54,7 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="row">
                         @foreach ($products as $product)
                             <div class="col-lg-4 col-md-6 col-sm-6 {{ $product->productCategory->slug }}">
@@ -72,28 +73,16 @@
                                 </div>
                             </div>
                         @endforeach
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="img/product/product-1.jpg">
-                                    <ul class="product__item__pic__hover">
-                                        <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
-                                </div>
-                                <div class="product__item__text">
-                                    <h6><a href="#">Crab Pool Security</a></h6>
-                                    <h5>$30.00</h5>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
+
                     <div class="product__pagination">
                         <a href="#">1</a>
                         <a href="#">2</a>
                         <a href="#">3</a>
                         <a href="#"><i class="fa fa-long-arrow-right"></i></a>
                     </div>
+
                 </div>
             </div>
         </div>
